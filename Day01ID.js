76fa9e6ef33cefc98966d6cb8efe2d008327e467
Prashant Bhattarai <prashantbhattarai567@gmail.com>
2026-06-04 23:43:56 -0500

    Remove practice files and keep only calculator

--- Day01ID.js
+++
@@ -1,8 +0,0 @@
-let name = "Prashant";
-let age = 21;
-let city = "Texas";
-
-console.log("===Profile Card===");
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("City:", city);
--- scripts.js
+++
@@ -1,3 +0,0 @@
-function myFunction3() {
-  document.getElementById("demo").innerHTML = "Paragraph changed succesfully.";
-}
